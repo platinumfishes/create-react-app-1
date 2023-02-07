9574da9096a03859efbe6f27694aada3b00a14ee
Platinumfish <mulerotmglel@gmail.com>
2023-02-07 14:25:56 -0500

    react push #1
    initial commit the thingy loads

--- src/App.js
+++ src/App.js
@@ -3,25 +3,20 @@ import './App.css';
 
 function App() {
   return (
-
 <>
-    <div class="outline">
-      <div class="lecard">
-        <h2>Some Nerd</h2>
-        <img src="https://i.kym-cdn.com/news_feeds/icons/mobile/000/037/359/05b.jpg" alt="Obi-Wan headshot" />
-        <p class="details">Hello there <br></br> Lightsaber go <i>brrrr</i></p>
-      </div>
+    <div class="topbuttons">
+      <button id="Duplicate">New duplicate</button>
+      <button id="Color">Toggle color</button>
+      <button id="Header">Change heading</button>
+      <button id="Delete">Delete 1 card</button>
     </div>
-  <div class="buttons">
-  
-  <button class="copyCat">Copy</button>
-  <button id="del">Delete</button>
-  <button class="btnDet">Toggle Details</button> 
-  <button class="btnCT">Change Title</button>
-  <button  class="btnBG">Change Background Color</button>
-  
-  </div>
-  
+      <div id="card1" class="card">
+      <p id="header1" class="header">Team Fortress 2 Pyroland</p>
+      <p id="bod1" class="bod">This is a screenshot from a 3D rendered animation posted to Youtube by Valve Corporation to promote the video-game Team Fortress 2</p>
+      <p id="bod2" class="bod">The video is called "Meet the Pyro"</p>
+      <img class="img" src="https://images.gamebanana.com/img/ss/mods/54f8f277e231a.webp" alt=""></img>
+      <button id="Details" class="button">Details</button>
+      </div>
 </>
   );
 }
